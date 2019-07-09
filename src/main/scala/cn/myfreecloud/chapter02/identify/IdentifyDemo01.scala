7f9b2437876e0adb5d30197987d8334a5c190902
zhangyang <969666310@qq.com>
2019-07-09 16:40:54 +0800

    scala额骚操作

--- src/main/scala/cn/myfreecloud/chapter02/identify/IdentifyDemo01.scala
+++ src/main/scala/cn/myfreecloud/chapter02/identify/IdentifyDemo01.scala
@@ -2,7 +2,29 @@ package cn.myfreecloud.chapter02.identify
 
 object IdentifyDemo01 {
   def main(args: Array[String]): Unit = {
-    //首字符可以为操作符
+    //首字符可以为操作符(比如 + -) 后续字符也需要跟操作符,只是由一个
     val ++  = "hello world"
+
+    println(++)
+
+    //因为底层已经做了编译阶段的处理,就不是真正的加减符号了
+
+    //勇 `` 的话,特殊字符也可以用作操作符
+
+    //scala额骚操作
+
+    var `true` = "hello"
+
+    println(`true`)
+
+    val Int = 60.33
+
+    val Float =123
+
+    println(Int)
+
+    println(Float)
+
+
   }
 }
